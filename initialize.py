9b585f6ed07ee4b179188544bc121e96431ef53a
Rohan Khanna <rohan.kh@gmail.com>
2020-11-05 16:43:53 +0100

    initialization script ready

---
+++ initialize.py
@@ -0,0 +1,8 @@
+from utils import *
+
+pg_connection = get_pg_connection()
+cursor = pg_connection.cursor()
+sql_file = open('schema.sql','r')
+cursor.execute(sql_file.read())
+cursor.close()
+pg_connection.commit()
